7560424dc0784211a7dff2e179f12c66ba5adcd2
andrewcalhoun <ajc7@illinois.edu>
2018-12-23 00:53:28 -0600

    Create PTR.py

---
+++ PTR.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Andrew Calhoun
+         University of Illinois at Urbana-Champaign
+Created on Sat Dec 8 2018
+Last edited on Sat Dec 8 2018
+"""
+
+import numpy as np
+from numba import jit
+
+"""'.source.python':
+  'Opening Comments':
+    'prefix': 'start'
+    'body': '# -*- coding: utf-8 -*-\n"""\n@author: Andrew Calhoun\n'+
+            '         University of Illinois at Urbana-Champaign\nCreated on Sat Dec 8 2018\nLast edited on Sat Dec 8 2018\n"""'"""
