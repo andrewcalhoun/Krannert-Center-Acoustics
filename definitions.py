ac4db7be53a03e42f6afd3502cf098780ba63318
andrewcalhoun <44107183+andrewcalhoun@users.noreply.github.com>
2019-01-12 10:29:06 -0600

    New Beginnings
    Implements the beginnings of the project structure and first programs. This should always work straight from the Github folder without any modifications per end user.

---
+++ definitions.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Andrew Calhoun
+         University of Illinois at Urbana-Champaign
+Created on Sat Jan 12 2019
+Last edited on Sat Jan 12 2019
+"""
+
+###This file is to contain any important information that must be shared
+###across multiple files in the Krannert-Center-Acoustics repository.
+
+
+import os
+
+
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__)) #Project root
