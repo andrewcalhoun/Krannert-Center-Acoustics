fee8b95059c879e4455e6bbef45a3062388b4f15
andrewcalhoun <ajc7@illinois.edu>
2019-01-21 02:58:41 -0600

    Multiple Runs
    This is an update to the infrastructure to have one file that runs the analysis (Sound_Decay.py) and another (Run.py) that calls the first for multiple files.

---
+++ Run.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Andrew Calhoun
+         University of Illinois at Urbana-Champaign
+Created on Jan 16 2019
+Last edited on Jan 16 2019
+"""
+
+import Sound_Decay as sd
+
+if __name__ == '__main__':
+
+    sd.main(0)
--- Sound_Decay.py
+++ Sound_Decay.py
@@ -2,8 +2,8 @@
 """
 @author: Andrew Calhoun
          University of Illinois at Urbana-Champaign
-Created on Sat Jan 12 2019
-Last edited on Sat Jan 12 2019
+Created on Jan 12 2019
+Last edited on Jan 17 2019
 """
 
 ###This file is used to calculate the rate of sound decay in recordings made in the Krannert Center Great Hall
@@ -31,19 +31,31 @@ def import_fnames():
         for filen in files:
             continue
 
-    fnames = files
-    print("fnames: "+fnames)
-
-    return fnames
+    return files
 
 
 def import_pickle(pname):
     """Easy to call function to import pickled Pandas DataFrames"""
 
+    pname = 'Data/'+pname #add the path into the filename
     data_master = pd.read_pickle(pname)
+    data_master = data_master.T.reset_index(drop=True).T #reset the column index so we have integer indices
+
     return data_master
 
 
-if __name__ == '__main__':
+def main(fnum):
+    """This takes the number assigned to the desired file and runs the analysis on it"""
 
     pnames = import_fnames() #return all names of the kDQ FFT data directory
+    print(pnames)
+
+    test = import_pickle(pnames[fnum])
+    print(test)
+
+
+if __name__ == '__main__':
+    """"This runs the whole code for one file"""
+    """This is broken up so Run.py can call main() across many files"""
+
+    main()
