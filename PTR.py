ff2f0ce27caca198802d8db711aa8a1a83058569
andrewcalhoun <ajc7@illinois.edu>
2019-01-21 02:42:59 -0600

    Housekeeping
    Updating some misc. items.

--- PTR.py
+++ PTR.py
@@ -14,6 +14,7 @@ import numpy as np
 import pandas as pd
 import os
 import sys
+from tqdm import tqdm
 
 #from numba import jit
 from definitions import ROOT_DIR
@@ -48,4 +49,9 @@ def import_pickle(pname):
 
 if __name__ == '__main__':
 
-    pnames = import_fnames() #return all names of the kDQ FFT data directory
+    #pnames = import_fnames() #return all names of the kDQ FFT data directory
+
+    l = ['this','is','a','list']
+
+    for word in tqdm(l):
+        print(word)
