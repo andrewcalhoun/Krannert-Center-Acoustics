514ee0dc2b1e0ff5d4a65f9ab3e66a17fdc987a3
andrewcalhoun <ajc7@illinois.edu>
2019-04-23 13:15:02 -0500

    Update definitions.py
    The import_fnames function now asks for a folder location. It returns a list of the file names contained in this folder rather than being hard-coded for the Data directory.

--- definitions.py
+++ definitions.py
@@ -3,7 +3,7 @@
 @author: Andrew Calhoun
          University of Illinois at Urbana-Champaign
 Created on Sat Jan 12 2019
-Last edited on Sat Mar 13 2019
+Last edited on Sun Apr 21 2019
 """
 
 ###This file is to contain any important information that must be shared
@@ -17,10 +17,9 @@ import sys
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__)) #Project root
 
 
-def import_fnames():
-    """This reads the data directory and creates a list of the file names"""
+def import_fnames(dataDirectory):
+    """This reads a file directory and creates a list of the file names"""
 
-    dataDirectory = ROOT_DIR + "/Data"
     if len(sys.argv) > 1:
         dataDirectory = sys.argv[1]
     ### find all kDQ pickled files by recursively walking the data directory
